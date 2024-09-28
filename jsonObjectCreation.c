014f350b9f7b393618aa007c38aef044f38813ea
Jeremiah <jeremiahjohn08706@gmail.com>
2024-09-28 15:13:17 -0500

    added C file for creation, parsing, and management of JSON file and its objects

---
+++ jsonObjectCreation.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+//when we create json object, it should also have status, createdAt, and updatedAt properties
+int createObject(int id, char **description);
+int updateObjectDesc(int id, char **description);
+int updateObjectStatus(int id, int status);
