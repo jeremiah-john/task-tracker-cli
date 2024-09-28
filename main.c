75aaedae92e41e883d0e387a2cf29a3b47d978b8
Jeremiah <jeremiahjohn08706@gmail.com>
2024-09-28 15:35:49 -0500

    ran successful test of argument parsing, dealing with main commands. must test now for parsing of integer arguments

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
-
+#include <string.h>
 int main(int argc, char **argv)
 {
+	//use strcmp(argv[i], "string") == 0 to compare command arguments
+	if(strcmp(argv[1],"add") == 0)
+	{
+		printf("successful add!\n");
+	}
+
+
+	if(strcmp(argv[1],"update") == 0)
+	{
+		printf("successful update!\n");
+	}
+	if(strcmp(argv[1],"delete") == 0)
+	{
+		printf("successful delete!\n");
+	}
+	if(strcmp(argv[1],"mark-in-progress") == 0)
+	{
+		printf("successful mark-in-progress!\n");
+	}
+	if(strcmp(argv[1],"mark-done") == 0)
+	{
+		printf("successful mark-done!\n");
+	}
+
+
+	if(strcmp(argv[1],"list") == 0)
+	{
+		printf("successful list!\n");
+	}
+
 	return 0;
 }
