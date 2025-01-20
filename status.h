ecddaa51dc46d2056a2849559f912e0d579cc558
Jeremiah <jeremiahjohn08706@gmail.com>
2025-01-20 11:40:40 -0600

    refactor: modified status enum to act as a flag. This makes the implementation of listTask easier to read

--- status.h
+++ status.h
@@ -1,11 +1,14 @@
 #ifndef STATUS_H
 #define STATUS_H
+// the Status enumeration values are bitshifted so that the code to list all tasks and tasks by status is easier to understand
+// essentially acting as flags as in C# code
 enum Status {
-	todo = 1,
-	inProgress = 2,
-	halted = 3,
-	done = 4,
+	todo = 1 << 0,
+	inProgress = 1 << 1,
+	halted = 1 << 2,
+	done = 1 << 3,
 	nullStatus = 0
 };
 int markTask(int taskIndex,enum Status newStatus);
+int listTask(enum Status searchStatus);
 #endif
